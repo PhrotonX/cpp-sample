ed4ac845450b9a475f8becc1c957dec94290d190
wonniebear <145176960+wonniebear@users.noreply.github.com>
2024-11-01 19:43:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@ int main()
 {
     while(true){
         cout << "Sample code here" << endl;
+     if (true){
+         cout<<"Universe";
+     }
+    else {
+        cout<<"Underworld";
+         }
     }
     
 //sample
